6d9aef97514fa00e29237319abc4d486fae8ce37
Admin <vanpho02@gmail.com>
2017-03-04 15:26:32 +0700

    init server express

---
+++ index.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+app.listen(3000, () => console.log('Server started'));
+
+app.get('/', (req, res) => res.render('home'));
