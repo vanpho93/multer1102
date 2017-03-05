c24ba45f4d0aea88bfb9b444e9fd9d30bed6eebe
Admin <vanpho02@gmail.com>
2017-03-05 09:30:55 +0700

    xong

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 let express = require('express');
 let app = express();
-
-let multer = require('multer'); //1
-
-let storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, './public');
-    },
-    filename: function (req, file, cb){
-        cb(null, req.body.username + Date.now() + file.originalname);
-    }
-});
-
-function fileFilter (req, file, cb) {
-    if(file.mimetype === 'image/png' 
-        || file.mimetype === 'image/jpg'){
-        return cb(null, true);
-    }
-    cb(new Error('Sai dinh dang file'));
-}
-
-let upload = multer({
-    storage, 
-    limits: {fileSize: 5 * 1024},
-    fileFilter
-}).single('avatar'); //1111
-
+let upload = require('./upload.js');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
