78a5ea7756457bf744ce6340027b152b0bacd8fa
Admin <vanpho02@gmail.com>
2017-03-05 11:22:50 +0700

    db

---
+++ db.js
@@ -0,0 +1,21 @@
+let pg = require('pg');
+
+let config = {
+  user: 'postgres',
+  database: 'NODE1102',
+  password: 'khoapham',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 1000
+}
+
+let pool = new pg.Pool(config);
+
+pool.connect((err, client, done) => {
+    if(err) return console.log(err + '');
+    client.query('SELECT * FROM "News"', (err, result) => {
+        if(err) return console.log(err + '');
+        console.log(result.rows);
+    });
+})
