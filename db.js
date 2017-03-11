22ecc4814379cab1aa92175c8352230b9af31140
Admin <vanpho02@gmail.com>
2017-03-11 13:57:14 +0700

    show data

--- db.js
+++ db.js
@@ -12,11 +12,11 @@ let config = {
 
 let pool = new pg.Pool(config);
 
-pool.connect((err, client, done) => {
-    if(err) return console.log(err + '');
-    client.query('SELECT * FROM "News"', (err, result) => {
-        done(err);
-        if(err) return console.log(err + '');
-        console.log(result.rows);
-    });
-})
+// pool.connect((err, client, done) => {
+//     if(err) return console.log(err + '');
+//     client.query('SELECT * FROM "News"', (err, result) => {
+//         done(err);
+//         if(err) return console.log(err + '');
+//         console.log(result.rows);
+//     });
+// });
--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 let express = require('express');
 let app = express();
+
+let pg = require('pg');
+
+let config = {
+  user: 'postgres',
+  database: 'NODE1102',
+  password: 'khoapham',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 1000
+}
+
+let pool = new pg.Pool(config);
+
 let upload = require('./upload.js');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+    pool.connect((err, client, done) => {
+        if(err) return res.send(err + '');
+        client.query('SELECT * FROM "News"', (err, result) => {
+            done(err);
+            if(err) return res.send(err + '');
+            res.send(result.rows);
+        });
+    });
+});
 
 app.post('/signup', (req, res) => {//2222
     upload(req, res, (err) => {
